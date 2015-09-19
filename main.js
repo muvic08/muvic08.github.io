e34e8ea000cafd4ee644af0a030a14954f6bbce6
muvic08 <muvic08@gmail.com>
2015-09-18 22:30:36 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    //get current location
+    var hash = window.location.hash;
+    console.log(hash);
+    if (hash) {
+    	var activeLink = $('a[href='+hash+']');
+    	$("#navbarCollapse > ul > li").removeClass("active");
+    	$(activeLink).parent().addClass("active");
+    	console.log(activeLink);
+    }
+
+    function goToByScroll(id){
+    	id = id.replace("link", "");
+      	// Scroll
+	    $('html, body').animate({scrollTop: $(id).offset().top - 50},'slow');
+	}
+
+	$("#navbarCollapse > ul > li > a").click(function(e) { 
+    	e.preventDefault(); 
+    	$("#navbarCollapse > ul > li").removeClass("active");
+    	$(this).parent().addClass("active");
+
+    	var id = $(this).attr('href');
+    	
+    	goToByScroll(id);   
+    	history.pushState({}, '', $(this).attr("href"));   
+
+    	$("#navbar-collapse-button").trigger("click");
+	});
+
+});
